659811d71611483a1538540038e6521726c8f627
phamkhoa1303 <pham.duykhoa1303@gmail.com>
2023-01-29 23:47:05 +0700

    push first source code

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/Final2.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/test.mp4",
+    },
+];
